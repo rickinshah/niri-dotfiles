d3f2ee38e62aedab33311a7012519d3bbd3c98ea
Rickin Shah <rickin.shah17403@gmail.com>
2025-06-25 18:43:43 +0530

    minor change in waybar hide script

--- .config/niri/scripts/waybar-auto-visibility.sh
+++ .config/niri/scripts/waybar-auto-visibility.sh
@@ -40,12 +40,15 @@ fi
 
 # Listen for events
 niri msg --json event-stream | while read -r line; do
-    if echo "$line" | jq -e '.WindowClosed or .WindowOpenedOrChanged or .WorkspaceActivated' >/dev/null; then
+    echo "$line"
+    if echo "$line" | jq -e '.WindowClosed or .WindowOpenedOrChanged or .WorkspaceActivated or .WindowFocusChanged' >/dev/null; then
         count=$(count_windows_in_current_workspace)
         if [[ $count -eq 0 ]]; then
             show_waybar
         else
             hide_waybar
         fi
+    elif echo "$line" | jq -e '.OverviewOpenedOrClosed | .is_open'; then
+        show_waybar
     fi
 done
